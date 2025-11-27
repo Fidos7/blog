b17ac97c2af0b6e1067ed0c0761fd5327ef4ea46
Adino <kozacek.adrian@gmail.com>
2025-11-27 20:34:33 +0100

    Initial commit

---
+++ posts.php
@@ -0,0 +1,42 @@
+<?php
+$posts = [
+    [
+        "id" => 1,
+        "title" => "Záhada zmizelé civilizace Göbekli Tepe",
+        "content" => "Göbekli Tepe v Turecku je starší než pyramidy i Stonehenge. Nikdo nechápe, 
+        jak lidé bez kovu dokázali zvednout obří kamenné monolity. Místo bylo navíc úmyslně zasypáno 
+        před tisíci lety – dodnes nevíme proč."
+    ],
+    [
+        "id" => 2,
+        "title" => "Nejrychlejší fotbalový hráč historie",
+        "content" => "Kylian Mbappé byl oficiálně změřen rychlostí 38 km/h, což je víc než mnozí 
+        profesionální sprinteři. Dribluje při plné rychlosti, což z něj dělá nejrychlejšího útočníka 
+        moderního fotbalu."
+    ],
+    [
+        "id" => 3,
+        "title" => "AI umí předvídat nemoci roky dopředu",
+        "content" => "Moderní AI dokáže z analýzy rentgenů, krve nebo srdečních dat předvídat 
+        nemoci i několik let dopředu. Některé modely mají přesnost až 90 % – vyšší než lidští odborníci."
+    ],
+    [
+        "id" => 4,
+        "title" => "Tesla Cybertruck má extrémně pevnou konstrukci",
+        "content" => "Karoserie Cybertrucku je z nerezové oceli typu 30X. Je tak tvrdá, 
+        že běžné lisovací stroje ji nedokázaly tvarovat. Tesla musela vyvinout vlastní postup 
+        ohýbání materiálu."
+    ],
+    [
+        "id" => 5,
+        "title" => "Nejzajímavější místo oceánu",
+        "content" => "V Tichém oceánu leží tzv. Point Nemo – místo nejdále od jakékoliv civilizace. 
+        Mezinárodní stanice ISS je často blíž lidem než ktokoliv poblíž Point Nemo."
+    ],
+    [
+        "id" => 6,
+        "title" => "Fotbalové derby, které slyší celá planeta",
+        "content" => "El Clásico mezi Barcelonou a Realem Madrid sleduje až 650 milionů lidí. 
+        Je to nejsledovanější klubový fotbalový zápas na světě."
+    ],
+];
